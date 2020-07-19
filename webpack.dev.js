9010e522fd5026804bb545f84fe3513784b199a2
Lam <daibanglam@gmail.com>
2020-07-19 16:07:27 +0700

    Proxy to backend

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const merge = require('webpack-merge');
 const webpack = require('webpack');
 const common = require('./webpack.common.js');
+var https = require('https');
 
 module.exports = merge(common, {
   mode: 'development',
@@ -14,7 +15,14 @@ module.exports = merge(common, {
     historyApiFallback: true,
     port: 3001,
     proxy: {
-      '/api': 'http://localhost:3000',
+      '/api': {
+        agent: https.globalAgent,
+        followRedirects: true,
+        target: 'https://www.metaweather.com',
+        headers: {
+          host: 'www.metaweather.com',
+        },
+      },
     },
   },
   plugins: [
