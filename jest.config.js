28ce77b4ae4f926c8eef51c0c58b8dd5d7ddadd9
Lam <daibanglam@gmail.com>
2020-07-25 14:30:52 +0700

    Add jest coverage threshold check

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,12 @@ module.exports = {
   // refer from create-react-app source code
   roots: ['<rootDir>/src'],
   setupFilesAfterEnv: ['<rootDir>/src/jest.setup.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
